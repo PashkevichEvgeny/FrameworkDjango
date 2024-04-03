378fb57a06699cb75fc02567c83998170f1a5489
PashkevichEvgeny <pash.ev@ya.ru>
2024-04-03 18:40:20 +0800

    Seminar 1 task 3

--- myproject/sem2app/models.py
+++ myproject/sem2app/models.py
@@ -13,5 +13,26 @@ class Coin(models.Model):
         name_side = 'Выпал ОРЕЛ' if self.side else 'Выпала РЕШКА'
         return f'{name_side}, время подбрасывания монеты {format(self.throw_time, "%m-%d %H:%M")}'
 
-    # def __repr__(self):
-    #     return f'Coin({self.side=}, {self.throw_time=})'
+    def __repr__(self):
+        return f'Coin({self.side=}, {self.throw_time=})'
+
+
+class Author(models.Model):
+    """## Задание №3
+    Создайте модель Автор.
+    Модель должна содержать следующие поля:
+    - имя до 100 символов
+    - фамилия до 100 символов
+    - почта
+    - биография
+    - день рождения
+    Дополнительно создай пользовательское поле “полное имя”, которое возвращает имя и фамилию.
+    """
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField()
+    biography = models.TextField()
+    dob = models.DateField()
+
+    def full_name(self):
+        return f'{self.name} {self.surname}'
