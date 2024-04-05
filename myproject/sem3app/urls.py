36592cd3e85f54d6373ebdae184b4e40af2a46ff
PashkevichEvgeny <pash.ev@ya.ru>
2024-04-06 00:38:02 +0800

    Seminar 3 task 3

--- myproject/sem3app/urls.py
+++ myproject/sem3app/urls.py
@@ -5,4 +5,7 @@ from .views import TemplateAbout
 urlpatterns = [
     path('main/', views.main, name='main'),
     path('about/', TemplateAbout.as_view(), name='about'),
+    path('head-tails/<int:throws>', views.head_tails, name='head_tails'),
+    path('dice/<int:throws>', views.dice, name='dice'),
+    path('rand100/<int:throws>', views.rand100, name='rand100')
 ]
--- myproject/sem3app/views.py
+++ myproject/sem3app/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
@@ -20,3 +22,25 @@ class TemplateAbout(TemplateView):
         context['message'] = 'Python'
         context['number'] = 'трех'
         return context
+
+
+def games(throws, size, message) -> dict:
+    return {'results': [f'{message} - {random.randint(*size)}' for _ in range(throws)]}
+
+
+def head_tails(request, throws):
+    context = games(throws, (0, 1), 'На монетке')
+    context.update({'title': 'Орел-Решка'})
+    return render(request, "sem3app/games.html", context)
+
+
+def dice(request, throws):
+    context = games(throws, (1, 6), 'Вам выпало')
+    context.update({'title': 'Кости'})
+    return render(request, "sem3app/games.html", context)
+
+
+def rand100(request, throws):
+    context = games(throws, (0, 100), 'Случайное число')
+    context.update({'title': 'Случайное число от 0 до 100'})
+    return render(request, "sem3app/games.html", context)
