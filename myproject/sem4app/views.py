8d32e03c9c70ddb040e76cff66a7812450a02e06
PashkevichEvgeny <pash.ev@ya.ru>
2024-04-08 17:09:06 +0800

    Seminar 4 task 2

--- myproject/sem4app/views.py
+++ myproject/sem4app/views.py
@@ -1,6 +1,9 @@
 import logging
+import random
 
 from django.shortcuts import render
+
+from sem3app.views import head_tails, dice, rand100
 from .forms import UserForm, GameForm
 
 logger = logging.getLogger(__name__)
@@ -26,11 +29,14 @@ def game_form(request):
         if form.is_valid():
             name_game = form.cleaned_data['name_game']
             throws = form.cleaned_data['throws']
-
+            match name_game:
+                case 'H':
+                    return head_tails(request, throws)
+                case 'D':
+                    return dice(request, throws)
+                case 'R':
+                    return rand100(request, throws)
             logger.info(f'Получили {name_game=}, {throws=}')
     else:
         form = GameForm()
     return render(request, 'sem4app/game_form.html', {'form': form})
-
-
-
