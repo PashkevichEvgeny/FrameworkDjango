c5710c7061895b4ff59be899e8de9815344ec3e7
PashkevichEvgeny <pash.ev@ya.ru>
2024-04-10 22:53:55 +0800

    Seminar 5 task 2

--- myproject/sem5app/admin.py
+++ myproject/sem5app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from sem3app.models import Author, Post, Comment
+from hw3app.models import Client, Product, Order
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Order)
