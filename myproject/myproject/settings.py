cbe80c34b736541280ab5e58439d843bd1eeaca5
PashkevichEvgeny <pash.ev@ya.ru>
2024-04-10 17:35:28 +0800

    Seminar 5 init

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sem5app',
     'hw4app',
     'sem4app',
     'hw3app',
@@ -116,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
