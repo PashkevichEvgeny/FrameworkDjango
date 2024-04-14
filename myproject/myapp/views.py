48eac8f06998ed51263f9bb9a26c71b9c9ac4237
PashkevichEvgeny <pash.ev@ya.ru>
2024-04-14 23:41:32 +0800

    Seminar 6 Homework

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,10 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+urls = {'sem3app/': ['main/',
+                     'about/',
+                     'head-tails/10',
+                     'dice/10',
+                     'rand100/10',
+                     'author/1',
+                     'post/1'],
+        'sem4app/': ['form/',
+                     'game/',
+                     'add-author/',
+                     'add-post/',
+                     'post/1'],}
 
 
 def index(request):
-    return HttpResponse('Привет МИР!')
+    return render(request, 'index.html', {'urls': urls})
 
 
 def about(request):
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,16 +20,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x^l(awdhg^7j-v6(5qmdsq^*wbds$jf==td5^hz4xqwx^nh&e1'
+# SECRET_KEY = 'django-insecure-x^l(awdhg^7j-v6(5qmdsq^*wbds$jf==td5^hz4xqwx^nh&e1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    '192.168.0.11',
+    # '192.168.0.11',
+    'geekbrains1404.pythonanywhere.com',
 ]
 
+INTERNAL_IPS = [
+     '127.0.0.1',
+]
 
 # Application definition
 
@@ -48,10 +56,12 @@ INSTALLED_APPS = [
     'hw2app',
     'sem2app',
     'myapp',
+    # 'debug_toolbar',
     # 'simplegamesapp',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -87,12 +97,28 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'geekbrains1404$default',
+        'USER': 'geekbrains1404',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'geekbrains1404.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
+
+
 
 
 # Password validation
@@ -130,6 +156,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
